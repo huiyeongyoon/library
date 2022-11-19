c9a9be934779bae164799d6abdd20c989de7131f
Huiyeong Yoon <zkdl2960@naver.com>
2022-11-19 12:23:16 +0900

    Revert "loading 기능 추가 크루셀 기능 추가중"
    This reverts commit 6e20b10baccaf459198f420c1190fb907eb800ee.

--- src/layouts/components/main.vue
+++ src/layouts/components/main.vue
@@ -6,38 +6,23 @@ el-main
         li(v-for="leftList in leftLists") 
           h1 {{ leftList }}
     .leftContainer
-      ul(v-infinite-scroll="fetchData" infinite-scroll-disabled="disabled")
+      ul(v-infinite-scroll="fetchData")
         li(v-for="(listData, index) in totalListData" @click="fetchList(listData, index)")
           .image-container
             img(:src="listData.image")
           .p-container 
             h1 project name: {{ listData.name }}
             p period: {{ listData.period }}
-      p(v-if="loading") Loading...
-      p(v-if="noMore") No more
   .main
     .top
       h1 {{ mainData.name }}
       p period: {{ mainData.period }}
-    .middle
-      //- el-carousel(:interval="1000" arrow="always") 
-        //- el-carousel-item(v-for=" item in listData")
-          //- p {{ item }} 
-          //- .image-container
-          //-   img(:src="item.image")
-          //- .p-container 
-          //-   h1 project name: {{ item.name }}
-          //-   p period: {{ item.period }}
-      
-      //- ul(v-infinite-scroll="fetchData" infinite-scroll-disabled="disabled")
-      //-     li(v-for="(listData, index) in totalListData" @click="fetchList(listData, index)")
-      //-       .image-container
-      //-         img(:src="listData.image")
-      //-       .p-container 
-      //-         h1 project name: {{ listData.name }}
-      //-         p period: {{ listData.period }}
+    .middle 
+      h1 {{ mainData.name }}
+      p period: {{ mainData.period }}
+      img(:src="mainData.image")
     .bottom
-      p xxxxx: aaaa bbb cccc
+      p 사용기술: 
 </template>
 
 <script>
@@ -55,46 +40,35 @@ export default {
   data() {
     return {
       totalListData: [],
-      defaultListDataCount: 10,
+      defaultListDataCount: 5,
       addedListData: 0,
       mainData: [],
-      loading: false
     }
   },
   mounted() {
     this.fetchData()
     this.fetchList()
   },
-  computed: {
-      noMore () {
-        return this.totalListData.length === this.listData.length
-      },
-      disabled () {
-        return this.loading || this.noMore
-      }
-    },
   methods: {
     fetchData() {
-      this.loading = true
-      setTimeout(() => {
-        let totalListDataCount = this.defaultListDataCount + this.addedListData
-        if (this.totalListData.length < this.listData.length) {
-          for (let i = this.addedListData; i < totalListDataCount; i++) {
-            if (totalListDataCount > this.totalListData.length) {
-              if (this.listData[i] !== undefined) {
-                this.totalListData.push(this.listData[i])
-              }
+      let totalListDataCount = this.defaultListDataCount + this.addedListData
+      if (this.totalListData.length < this.listData.length) {
+        for (let i = this.addedListData; i < totalListDataCount; i++) {
+          if (totalListDataCount > this.totalListData.length) {
+            if (this.listData[i] !== undefined) {
+              this.totalListData.push(this.listData[i])
             }
           }
-          this.addedListData += 10
         }
-          this.loading = false
-        }, 500)
+        this.addedListData += 5
+      }
     },
-    fetchList(currentData) {
-      this.mainData = this.listData[0]
-      if (currentData) {
-        this.mainData = currentData
+    fetchList(listData) {
+      if (listData === undefined) {
+        this.mainData = this.totalListData[0]
+      }
+      if (listData) {
+        this.mainData = listData
       }
     },
   },
@@ -145,22 +119,6 @@ export default {
         padding: 5px 0;
       }
     }
-  }
-  .main {
-    .middle {
-      // .el-carousel__item h3 {
-      //   color: #475669;
-      //   font-size: 18px;
-      //   opacity: 0.75;
-      //   line-height: 300px;
-      //   margin: 0;
-      // }
-    }
-  }
-  p {
-    text-align: center;
-    font-size: 15px;
-    padding: 20px;
   }
 }
 </style>
